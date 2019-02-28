ab5217fa634380c805ef19f7752f89fb72150ea0
liqingfeng <qingfeng-love@qq.com>
2019-02-28 20:00:22 +0800

    完成界面布局优化

--- app/src/main/java/com/github/StormWyrm/wanandroid/ui/project/adapter/ProjectCategoryAdapter.kt
+++ app/src/main/java/com/github/StormWyrm/wanandroid/ui/project/adapter/ProjectCategoryAdapter.kt
@@ -6,15 +6,16 @@ import com.chad.library.adapter.base.BaseViewHolder
 import com.github.StormWyrm.wanandroid.R
 import com.github.StormWyrm.wanandroid.bean.project.ProjectDataItem
 
-class CategoryAdapter : BaseQuickAdapter<ProjectDataItem, BaseViewHolder>(R.layout.item_home, null) {
+class FragmentCategoryAdapter : BaseQuickAdapter<ProjectDataItem, BaseViewHolder>(R.layout.item_chapter_detail, null) {
     init {
         openLoadAnimation()//开启加载动画
     }
 
     override fun convert(helper: BaseViewHolder?, item: ProjectDataItem?) {
-        helper?.setText(R.id.tvTitle, item?.title)
-            ?.setText(R.id.tvAuthor, item?.author)
-            ?.setText(R.id.tvOrigin, item?.superChapterName)
-            ?.setText(R.id.tvPushlishTime, DateFormat.format("yyyy-MM-dd", item?.publishTime ?: 0))
+        helper?.run{
+            setText(R.id.tvTitle, item?.title)
+            setText(R.id.tvPushlishTime, DateFormat.format("yyyy-MM-dd", item?.publishTime ?: 0))
+        }
+
     }
 }
