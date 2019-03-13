c3cfdb19019f13d75f535f1f6a6602e574aa467c
liqingfeng <qingfeng-love@qq.com>
2019-03-13 16:31:11 +0800

    域名切换到Https“

--- app/src/main/java/com/github/StormWyrm/wanandroid/apis/WanAndroidRetrofitHelper.kt
+++ app/src/main/java/com/github/StormWyrm/wanandroid/apis/WanAndroidRetrofitHelper.kt
@@ -3,6 +3,6 @@ package com.github.StormWyrm.wanandroid.apis
 import com.github.StormWyrm.wanandroid.utils.RetrofitHelper
 
 object WanAndroidRetrofitHelper : RetrofitHelper<WanAndroidApis>() {
-    override val baseUrl: String = "http://www.wanandroid.com/"
+    override val baseUrl: String = "https://www.wanandroid.com/"
     override fun getClazz(): Class<WanAndroidApis> = WanAndroidApis::class.java
 }
